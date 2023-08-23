9bcfbcadb4c346c5adeecdc4259da2501c0c9cd1
Lefteris Vatsithianos D1 <digital.failure12@gmail.com>
2023-08-23 19:44:10 +0300

    Struct Trapezoid

--- src/integrate/integrator.rs
+++ src/integrate/integrator.rs
@@ -1,61 +1,58 @@
 
 
-pub struct DxIntegratorArgs{
-
-    //function: Fn(f64)->f64;
-    //a:f64;
-    //b:f64;
-    n:usize
-
-}
-
-impl Default for DxIntegratorArgs{
-
-    fn default() -> Self {
-        DxIntegratorArgs { n:1000 }
-}
-}
-
-// does not give number of points
-impl From<()> for DxIntegratorArgs {
-    fn from(_: ()) -> Self {
-        Self::default()
-    }
-}
-
-//gives number of points
-impl From<usize> for DxIntegratorArgs { 
-    fn from(n: usize) -> Self {
-        Self {
-            n:n,
-            ..Self::default()
-        }
-    }
-}
 
 
-pub fn composite_trapezoid<F,N>(function: F, a: f64, b: f64, arg_like:N) -> f64
-
-
-where F: Fn(f64) -> f64, N: Into<DxIntegratorArgs>,
+pub struct CompositeTrapezoid<F>
 {
+    function: F,
+    a: f64,
+    b: f64,
+    
+    size: usize
 
-    let args= arg_like.into();
-    let size=args.n;
-    //declare step size
-    let h = (b - a) / size as f64;
+}
 
-    let mut result = 0.5 * (function(a) + function(b));
+impl<F> CompositeTrapezoid<F>
+where
+    F:Fn(f64)->f64,
+{
+    pub fn initialize(function:F,a:f64,b:f64)-> Self {
+        Self { 
 
-    for i in 1..size {
-        let x = a + i as f64 * h;
-        result += function(x);
+    
+            function,
+            a,
+            b,
+            size:10000 }
+
+
     }
 
-    result *= h;
-    return result;
+    pub fn steps (mut self, size:usize) -> Self {
+
+        self.size=size;
+        self
+
+        
+
+    }
+
+    pub fn run (self)->f64 {
 
 
+        let h = (self.b - self.a) / self.size as f64;
+
+        let mut result = 0.5 * (self.function)(self.a) + (self.function)(self.b);
+
+        for i in 1..self.size {
+            let x = self.a + i as f64 * h;
+            result += (self.function)(x);
+        }
+
+        result *= h;
+        return  result;
+    }
+   
 
 }
 
